c156fa149777c218249608109edd510febfd25ac
Сергей Малюгов <gr402_maspe@lab-407-4.tomtit-tomsk.ru>
2024-01-16 12:16:06 +0700

    создание формы

--- js/main.js
+++ js/main.js
@@ -5,10 +5,30 @@ let eventBus = new Vue()
 Vue.component("board", {
     template: `
 <div class="board">
-<ul>
-<li><column></column></li>
-<li><column></column></li>
-<li><column></column></li>
+<ul id="columns">
+<li class="column">
+<form @submit.prevent="onSubmit">
+<label for="name">Заголовок</label> <input type="text" id="name" v-model="name"> 
+
+<label for="point1">Пункт1</label> <input type="text" id="point1" v-model="point1"> 
+<label for="point2">Пункт2</label> <input type="text" id="point2" v-model="point2"> 
+<label for="point3">Пункт3</label> <input type="text" id="point3" v-model="point3"> 
+<label for="point4">Пункт4</label> <input type="text" id="point4" v-model="point4"> 
+<label for="point5">Пункт5</label> <input type="text" id="point5" v-model="point5"> 
+
+<button type="submit" value="Submit">Создать</button>
+
+
+</form>
+
+
+</li>
+
+
+<li class="column">qwe</li>
+
+
+<li class="column">qeqw</li>
 
 
 </ul>
@@ -16,20 +36,50 @@ Vue.component("board", {
     `,
     data() {
         return{
-            name:null
+            column1:[],
+            column2:[],
+            column3:[],
+
+            name:null,
+            point1:null,
+            point2:null,
+            point3:null,
+            point4:null,
+            point5:null,
+            
+            points:[],
+
+            error:[]
+
+        }
+    },
+    methods:{
+        onSubmit(){
+            this.points=[]
+            if(this.point1){
+                this.points.push(this.point1)
+            }
+            if(this.point2){
+                this.points.push(this.point2)
+            }
+            if(this.point3){
+                this.points.push(this.point3)
+            }
+            if(this.point4){
+                this.points.push(this.point4)
+            }
+            if(this.point5){
+                this.points.push(this.point5)
+            }
+            
+
+            console.log(this.points)
         }
     }
 });
-Vue.component("column", {
-    template: `
-<div class="column">qweqwe</div>
-    `,
-    data() {
-        return{
-            name:null
-        }
-    }
-});
+
+
+
 
 
 Vue.component("task", {
@@ -38,7 +88,9 @@ Vue.component("task", {
     `,
     data() {
         return{
-            name:null
+            name:null,
+            points:[],
+            
         }
     }
 });
